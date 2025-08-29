e74d3254190135a86e40991d95a5750f712c6569
glebholopov <holopovg0@gmail.com>
2025-08-29 22:18:58 +0300

    taskTwo

--- src/TaskTwo.java
+++ src/TaskTwo.java
@@ -1,2 +1,22 @@
 public class TaskTwo {
+    public static void main(String[] args) {
+        String[][] array = new String[8][8];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if ((i + j) % 2 == 0) {
+                    array[i][j] = "W";
+                } else {
+                    array[i][j] = "B";
+                }
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
